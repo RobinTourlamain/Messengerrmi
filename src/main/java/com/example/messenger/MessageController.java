9b03a776faa69ddd3fed7909813f3681c456b3a8
RobinTourlamain <robin.tourlamain@gmail.com>
2022-10-06 13:15:21 +0200

    klassen ingevuld

--- src/main/java/com/example/messenger/MessageController.java
+++ src/main/java/com/example/messenger/MessageController.java
@@ -5,6 +5,9 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class MessageController {
     public Button Btn_client1;
     public Button Btn_client2;
@@ -12,6 +15,8 @@ public class MessageController {
     public Button Btn_send;
     public TextField Txtfld_incoming;
     public TextField Txtfld_outgoing;
+    public MessageClient messageClient = new MessageClient();
+
 
 
     @FXML
@@ -25,6 +30,8 @@ public class MessageController {
 
     }
     public void onSendButtonClick(){
-
+        PrintWriter out = messageClient.getWriter();
+        out.println(Txtfld_outgoing.getText());
+        Txtfld_outgoing.clear();
     }
 }
