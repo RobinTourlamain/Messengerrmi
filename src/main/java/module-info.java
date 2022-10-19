d4e65bdb119776393a5d0785941b4f426b90f7cb
RobinTourlamain <robin.tourlamain@gmail.com>
2022-10-19 14:29:18 +0200

    Changes by renautmestdagh

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,7 @@ module com.example.messenger {
 
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
+    requires java.rmi;
 
     opens com.example.messenger to javafx.fxml;
     exports com.example.messenger;
